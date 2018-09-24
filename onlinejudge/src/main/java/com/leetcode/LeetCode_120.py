8d468097a0a5663ee697750ebb68ea85104ccf3e
Zewen Xu <zw.final@gmail.com>
2018-09-24 15:30:31 +1000

    finish two

--- onlinejudge/src/main/java/com/leetcode/LeetCode_120.py
+++ onlinejudge/src/main/java/com/leetcode/LeetCode_120.py
@@ -4,3 +4,24 @@ class Solution:
         :type triangle: List[List[int]]
         :rtype: int
         """
+        if len(triangle) == 0:
+            return 0
+        if len(triangle) == 1:
+            return triangle[0][0]
+
+        pre = [triangle[0][0]]
+        for i in range(1, len(triangle)):
+            now = [triangle[i][0] + pre[0]]
+            for j in range(1, len(triangle[i]) - 1):
+                now.append(min(pre[j] + triangle[i][j], pre[j - 1] + triangle[i][j]))
+            now.append(triangle[i][-1] + pre[-1])
+            pre = now
+        ans = now[0]
+        for i in now:
+            ans = min(i, ans)
+        return ans
+
+
+input = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]
+
+print(Solution().minimumTotal(input))
--- onlinejudge/src/main/java/com/leetcode/LeetCode_300.py
+++ onlinejudge/src/main/java/com/leetcode/LeetCode_300.py
@@ -1,4 +1,4 @@
-# Input: [10,9,2,5,3,7,101,18]
+# Input: [8,9,2,5,3,7,101,18]
 # Output: 4
 # Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
 
@@ -8,3 +8,18 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
+        if len(nums) == 0:
+            return 0
+
+        f = [1] * len(nums)
+        max_num = 1
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    f[i] = max(f[j] + 1, f[i])
+                    if f[i] > max_num:
+                        max_num = f[i]
+        return max_num
+
+
+print(Solution().lengthOfLIS([8, 9, 2, 5, 3, 7, 101, 111, 19, 18]))
